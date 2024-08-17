48f79c0fb08fc0af3879f53a3838cd33c02e062b
Rahsya Benova <111744954+RahsyaBenova@users.noreply.github.com>
2024-08-17 22:29:31 +0700

    Update ngrok app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ mqtt_client.loop_start()
 valve_state = False
 
 # Set IP ESP32 
-ESP32_IP = 'https://115a-36-68-26-155.ngrok-free.app/'
+ESP32_IP = 'https://a31a-36-68-30-79.ngrok-free.app/'
 
 # Fetch total volume from MongoDB (from history collection)
 def fetch_total_volume():
